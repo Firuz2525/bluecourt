17bffb146d747dc25025ab94faef0d84b2aa0357
Abdulloh <boburkadirov2525@gmail.com>
2026-01-15 18:58:19 +0500

    env file made

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,15 +1,7 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-const firebaseConfig = {
-  apiKey: "AIzaSyDyYnlzugCVxQF1xdbny_3YOZOHL37uQ2g",
-  authDomain: "bobhotel-caba0.firebaseapp.com",
-  projectId: "bobhotel-caba0",
-  storageBucket: "bobhotel-caba0.firebasestorage.app",
-  messagingSenderId: "237096103164",
-  appId: "1:237096103164:web:37b1c32d023a7bfd143b79",
-  measurementId: "G-XDYDK0QPQR",
-};
+// import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+// import { getFirestore } from "firebase/firestore";
+
 // const firebaseConfig = {
 //   apiKey: process.env.NEXT_PUBLIC_APIKEY,
 //   authDomain: process.env.NEXT_PUBLIC_AUTHDOMAIN,
@@ -19,6 +11,25 @@ const firebaseConfig = {
 //   appId: process.env.NEXT_PUBLIC_APPID,
 // };
 
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+// export const auth = getAuth(app);
+// export const db = getFirestore(app);
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Prevent re-initialization (VERY IMPORTANT in Next.js)
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
